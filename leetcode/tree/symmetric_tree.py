575f2ef4373e1c053678dfc494edef7565e0b068
Tinh Nguyen <tinhtn1508@gmail.com>
2020-05-05 20:28:54 +0700

    Fix adding common

--- leetcode/tree/symmetric_tree.py
+++ leetcode/tree/symmetric_tree.py
@@ -1,9 +1,4 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from common import TreeNode
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
         def isMirror(t1: TreeNode, t2: TreeNode) -> bool:
