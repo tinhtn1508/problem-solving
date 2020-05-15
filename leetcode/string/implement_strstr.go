4acda9ecc56fec62db583a6dbddbcaa403623d1c
Tinh Nguyen <tinhtn1508@gmail.com>
2020-05-15 23:08:11 +0700

    Add implement strstr

---
+++ leetcode/string/implement_strstr.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func strstr(haystack, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(strstr("hello", "ll"))
+}
