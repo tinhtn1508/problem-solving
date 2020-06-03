bcf39ee409cc06bf4dad1d5074e43eee2bc74513
Tinh Nguyen <tinhtn1508@gmail.com>
2020-06-03 22:50:33 +0700

    Add merge k sorted list problem

---
+++ leetcode/collect/merge_k_sorted_lists.py
@@ -0,0 +1,41 @@
+from queue import PriorityQueue
+from typing import List
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    
+    def __lt__(self, other):
+        return self.val < other.val
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put((l.val, l))
+        while not q.empty():
+            val, node = q.get()
+            point.next = ListNode(val)
+            point = point.next
+            node = node.next
+            if node:
+                q.put((node.val, node))
+        return head.next
+
+if __name__ == "__main__":
+    l1 = ListNode(1)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(5)
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(4)
+    l3 = ListNode(2)
+    l3.next = ListNode(6)
+    res = Solution().mergeKLists([l1, l2, l3])
+    
+    while res:
+        print(res.val)
+        res = res.next
